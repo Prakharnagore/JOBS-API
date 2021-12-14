e229f09b066bc156faf9074ac0c215438226a907
prakharn111 <prakharnagore111@gmail.com>
2021-12-14 12:54:53 +0530

    first commit

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -25,7 +25,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err.name === "CastError") {
     customError.msg = `No item Found with id:${err.value}`;
     customError.statusCode = 404;
-  }
+  }   
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: customError.msg });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
